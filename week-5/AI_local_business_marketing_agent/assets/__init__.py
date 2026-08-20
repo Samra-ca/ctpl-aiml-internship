2aea79c1e6847fc68483064e7391167bec078d80
Samra-ca <sminhas1405@gmail.com>
2026-08-20 19:02:34 +0500

    Clear Week 5 project files

--- week-5/AI_local_business_marketing_agent/assets/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Assets Package - Static assets (images, logos, etc.)
-"""
-
-from pathlib import Path
-
-ASSETS_DIR = Path(__file__).parent
-
-__all__ = ['ASSETS_DIR']
--- week-5/AI_local_business_marketing_agent/data/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Data Package - Data storage and management
-"""
-
-from pathlib import Path
-
-DATA_DIR = Path(__file__).parent
-
-__all__ = ['DATA_DIR']
